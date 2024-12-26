d32e852f74a94b26697987dd43721c2f77b513f3
Семён Шалагинов <semashal@yandex.ru>
2024-12-26 17:39:49 +0800

    Major fixes

--- RouteProgram.py
+++ RouteProgram.py
@@ -1,7 +1,23 @@
-from importlib.metadata import pass_none
-
 from TSProgram import TravelingSalesman
 from tabulate import tabulate
+
+def start_generation(actual_locs):
+    TS = TravelingSalesman(actual_locs)
+    coords = TS.get_coords()
+    matrix = TS.calculate_distance_matrix(coords)
+    print("Матрица расстояний")
+    print(tabulate(matrix, tablefmt="grid"))
+    length, path = TS.solve()
+    print(f"Минимальная длина маршрута: {length}")
+    print("Маршрут:")
+    route_coords = []
+    for i in path:
+        print(actual_locs[i], " -> ", end='')
+        route_coords.append(coords[i])
+    print('\n', "Генерация карты...")
+    TS.build_route_multiple_points(route_coords)
+    actual_locs.clear()
+    actual_locs.append(whouse_loc)
 
 print("Введите адрес склада")
 whouse_loc = input()
@@ -22,26 +38,35 @@ while True:
     for i, loc in enumerate(locs):
         print(f"{i+1}. ", loc)
     print("Для выхода введите 0.", "Для отправления грузовика напишите 'go' ", sep='\n')
-    inp = int(input())
+    inp = input()
+    if inp == "go":
+        if len(actual_locs)>1:
+            start_generation(actual_locs)
+            cur_weight = 0
+            continue
+        else:
+            print("Невозможно отправить грузовик")
+            continue
+    inp = int(inp)
     if inp==0:
         break
     else:
-        if locs[inp - 1] not in actual_locs:
-            actual_locs.append(locs[inp-1])
-        if inp!="go":
-            print("Введите вес запрашиваемого груза")
-            weight = int(input())
-        else:
-            print("Генерация маршрута...")
-        if cur_weight+weight < max_weight and inp!='go':
+        print("Введите вес запрашиваемого груза")
+        weight = int(input())
+        if cur_weight+weight < max_weight:
             cur_weight+=weight
+            if locs[inp - 1] not in actual_locs:
+                actual_locs.append(locs[inp - 1])
             continue
         if cur_weight+weight == max_weight:
             cur_weight = 0
+            if locs[inp - 1] not in actual_locs:
+                actual_locs.append(locs[inp - 1])
             print("Грузовик полностью заполнен. Генерация маршрута...")
         elif cur_weight+weight > max_weight:
             cur_weight=weight
             print("Груз не помещается в грузовик. Данный груз будет отправлен в следующем грузовике. Генерация маршрута...")
+
         TS = TravelingSalesman(actual_locs)
         coords = TS.get_coords()
         matrix = TS.calculate_distance_matrix(coords)
